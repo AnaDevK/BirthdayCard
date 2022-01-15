0ed2f4f202d8386496ff0ec36aba5bcf655cda52
AnaDevK <parvu.anamaria@gmail.com>
2022-01-15 17:05:43 +0200

    BirthdayCards v3

--- app/src/main/java/com/example/happybirthday/CardActivity.kt
+++ app/src/main/java/com/example/happybirthday/CardActivity.kt
@@ -23,17 +23,19 @@ import android.widget.*
 import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
-import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.FileOutputStream
-import java.util.*
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.happybirthday.databinding.ActivityCardBinding
+import java.io.IOException
 
 class CardActivity : AppCompatActivity() {
     private lateinit var txtHb: TextView
     private lateinit var txtWish: TextView
     private lateinit var imageView: ImageView
     private lateinit var cardView: CardView
+    private lateinit var binding: ActivityCardBinding
 
     var mMediaPlayer: MediaPlayer? = null
     var title = ""
@@ -44,17 +46,40 @@ class CardActivity : AppCompatActivity() {
     var bgColor = 0
     var saved = false
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                val card = findViewById<CardView>(R.id.cardView)
+                if (!saved) {
+                    imageUri = saveImage(card, this)
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Already saved on BirthdayCards folder.",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+                Log.i("Permission: ", "Granted")
+            } else {
+                Log.i("Permission: ", "Denied :(")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_card)
+        binding = ActivityCardBinding.inflate(layoutInflater)
+
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        txtHb = findViewById(R.id.txtHb)
-        txtWish = findViewById(R.id.txtWish)
-        imageView = findViewById(R.id.imageView)
-        cardView = findViewById(R.id.cardView)
-
+        txtHb = binding.txtHb
+        txtWish = binding.txtWish
+        imageView = binding.imageView
+        cardView = binding.cardView
+        setContentView(cardView)
         ContextCompat.getColor(this, R.color.black)
 
         val bundle = intent.extras
@@ -73,15 +98,15 @@ class CardActivity : AppCompatActivity() {
         txtHb.setTextColor(textColor)
         txtWish.setTextColor(textColor)
         cardView.setCardBackgroundColor(bgColor)
-        playSound()
+        //playSound()
         if (message != "") txtWish.text = message
         if (imageUri == null) {
             imageUri = Uri.Builder()
-                    .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-                    .authority(resources.getResourcePackageName(R.drawable.cake))
-                    .appendPath(resources.getResourceTypeName(R.drawable.cake))
-                    .appendPath(resources.getResourceEntryName(R.drawable.cake))
-                    .build()
+                .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                .authority(resources.getResourcePackageName(R.drawable.cake))
+                .appendPath(resources.getResourceTypeName(R.drawable.cake))
+                .appendPath(resources.getResourceEntryName(R.drawable.cake))
+                .build()
         }
         Picasso.get().load(imageUri).into(imageView)
         //if(imageUri != null) imageView.setImageURI()
@@ -133,14 +158,11 @@ class CardActivity : AppCompatActivity() {
         }
         if (id == R.id.action_share) {
             //Save image to be able to share it
-            if (!saved) {
-                val card = findViewById<CardView>(R.id.cardView)
-                imageUri = saveImage(card, this)
-            }
+            checkPermissionsAndSave()
             val intent = Intent(Intent.ACTION_SEND)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             intent.putExtra(Intent.EXTRA_STREAM, imageUri)
-            intent.type = "image/png"
+            intent.type = "image/*"
             startActivity(Intent.createChooser(intent, "Share image via"))
             return true
         }
@@ -150,7 +172,8 @@ class CardActivity : AppCompatActivity() {
     private fun getScreenShotFromView(v: View): Bitmap? {
         var screenshot: Bitmap? = null
         try {
-            screenshot = Bitmap.createBitmap(v.measuredWidth, v.measuredHeight, Bitmap.Config.ARGB_8888)
+            screenshot =
+                Bitmap.createBitmap(v.measuredWidth, v.measuredHeight, Bitmap.Config.ARGB_8888)
             val canvas = Canvas(screenshot)
             v.draw(canvas)
         } catch (e: Exception) {
@@ -160,111 +183,130 @@ class CardActivity : AppCompatActivity() {
     }
 
     fun saveImage(itemImage: View, activity: Activity): Uri? {
-        var fileName: String
-        val imageFromView = getScreenShotFromView(itemImage)
-        var bitmap: Bitmap
+        val fileName: String
+        val bitmap = getScreenShotFromView(itemImage)
 
-        ByteArrayOutputStream().apply {
-            bitmap = Bitmap.createBitmap(imageFromView!!)
-            fileName = UUID.randomUUID().toString()
-        }
+        val timeStamp: String = System.currentTimeMillis().toString()
+        fileName = "BdCard$timeStamp"
 
-        val directory = File("${getExternalFilesDir(Environment.DIRECTORY_PICTURES)}/BirthdayCards/").apply {
-            if (!exists())
-                mkdirs()
-        }
-        // For devices running android >= Q
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            // Getting the contentResolver
-            this.contentResolver?.also { resolver ->
-
-                // Content resolver will process the contentvalues
-                val contentValues = ContentValues().apply {
-
-                    // putting file information in content values
-                    put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
-                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
-                    put(MediaStore.MediaColumns.RELATIVE_PATH, "${getExternalFilesDir(Environment.DIRECTORY_PICTURES)}/BirthdayCards/")
-                }
-
-                // Inserting the contentValues to contentResolver and getting the Uri
-                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-
-                // Opening an outputstream with the Uri that we got
-                val fos = imageUri?.let { resolver.openOutputStream(it) }
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-                fos!!.flush()
-                fos!!.close()
+        //Create a folder to save birthdays cards
+        val directory =
+            File("${getExternalFilesDir(Environment.DIRECTORY_PICTURES)}/BirthdayCards/").apply {
+                if (!exists())
+                    mkdirs()
             }
+
+        if (savePhotoToExternalStorage(fileName, bitmap!!, directory)) {
+            Toast.makeText(activity, "Saved on BirthdayCards folder.", Toast.LENGTH_SHORT).show()
+            saved = true
+            return Uri.parse(directory.absolutePath + "/$fileName.jpg")
         } else {
-            File(directory, "$fileName.jpg").apply {
-                FileOutputStream(this).apply {
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, this)
-                    flush()
-                    close()
-                }
-            }.let {
-                activity.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                        ContentValues().apply {
-                            put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis())
-                            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-                            put(MediaStore.Images.Media.DATA, it.absolutePath)
-                        }
+            Toast.makeText(activity, "Error saving the image.", Toast.LENGTH_SHORT).show()
+            return null
+        }
+    }
+
+    private fun savePhotoToExternalStorage(name: String, bmp: Bitmap, path: File): Boolean {
+        val imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "$name")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            put(MediaStore.Images.Media.WIDTH, bmp.width)
+            put(MediaStore.Images.Media.HEIGHT, bmp.height)
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(
+                    MediaStore.Images.Media.RELATIVE_PATH,
+                    Environment.DIRECTORY_PICTURES + "/BirthdayCards/"
+                )
+            } else {
+                val file = File(path, "$name.jpg")
+                put(
+                    MediaStore.Images.Media.DATA, file.absolutePath
                 )
             }
         }
-        Toast.makeText(activity, "Saved on BirthdayCards folder.", Toast.LENGTH_SHORT).show()
-        saved = true
-        return Uri.parse(directory.absolutePath)
-    }
-
-    private fun hasWriteExternalStoragePermission() = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-    private fun hasReadExternalStoragePermission() = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-
-    private fun checkPermissionsAndSave() {
-        var permissionsToRequest = mutableListOf<String>()
-        if (!hasWriteExternalStoragePermission()) {
-            permissionsToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        }
-
-        if (!hasReadExternalStoragePermission()) {
-            permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-        }
-
-        if (permissionsToRequest.isNotEmpty()) {
-            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
-        } else {
-            val card = findViewById<CardView>(R.id.cardView)
-            if (!saved) {
-                imageUri = saveImage(card, this)
-            } else {
-                Toast.makeText(this, "Already saved on BirthdayCards folder.", Toast.LENGTH_SHORT)
-                        .show()
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 0 && grantResults.isNotEmpty()) {
-            for (i in grantResults.indices) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("PermissionsRequest", "${permissions[i]} granted")
-                    val card = findViewById<CardView>(R.id.cardView)
-                    if (!saved) {
-                        imageUri = saveImage(card, this)
+        return try {
+            contentResolver.insert(imageCollection, contentValues)?.also { uri ->
+                contentResolver.openOutputStream(uri).use { outputStream ->
+                    if (!bmp.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)) {
+                        throw IOException("Couldn't save bitmap")
                     }
-                } else {
-                    Toast.makeText(this, "No access to media files, can't save picture.", Toast.LENGTH_SHORT)
-                            .show()
-                    return
                 }
-            }
+            } ?: throw IOException("Couldn't create MediaStore entry")
+            true
+        } catch (e: IOException) {
+            Log.e("TAG", "Failed to save image because:" + e.message)
+            false
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
+    }
+
+    fun checkPermissionsAndSave() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                val card = findViewById<CardView>(R.id.cardView)
+                if (!saved) {
+                    imageUri = saveImage(card, this)
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Already saved on BirthdayCards folder.",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) -> {
+                cardView.showSnackbar(
+                    cardView,
+                    getString(R.string.permission_required),
+                    Snackbar.LENGTH_INDEFINITE,
+                    getString(R.string.ok)
+                ) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                }
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            }
+        }
+    }
+}
+
+fun View.showSnackbar(
+    view: View,
+    msg: String,
+    length: Int,
+    actionMessage: CharSequence?,
+    action: (View) -> Unit
+) {
+    val snackbar = Snackbar.make(view, msg, length)
+    if (actionMessage != null) {
+        snackbar.setAction(actionMessage) {
+            action(this)
+        }.show()
+    } else {
+        snackbar.show()
     }
 }
